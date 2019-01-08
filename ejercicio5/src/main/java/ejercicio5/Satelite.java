16324389fcee9207242f5951cf877b13c624e0d7
oleksiiolefir <46476855+oleksiiolefir@users.noreply.github.com>
2019-01-08 08:57:12 +0100

    ejercicio5

---
+++ ejercicio5/src/main/java/ejercicio5/Satelite.java
@@ -0,0 +1,31 @@
+package ejercicio5;
+
+public class Satelite {
+
+	
+	
+	
+		public double meridiano;
+		public double paralelo;
+		public double distancia_tierra;
+		
+		public Satelite (double m,double p,double d){
+			this.meridiano=m;
+			this.paralelo=p;
+			this.distancia_tierra=d;
+		}
+		public Satelite (){
+			this.meridiano=paralelo=distancia_tierra=0;
+		}
+		public void setPosicion(double m,double p,double d){
+			this.meridiano=m;
+			this.paralelo=p;
+			this.distancia_tierra=d;
+		}
+		
+		public void printPosicion(){
+			System.out.println("El satelite se encuentra en el paralelo"+this.paralelo+"Meridiano"+this.meridiano+"a una distancioa de la tierra de "+this.distancia_tierra+"Kilometros");
+			}
+	}
+
+
---
+++ ejercicio5/src/test/java/ejercicio5/testEjercicio5.java
@@ -0,0 +1,49 @@
+package ejercicio5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testEjercicio5 {
+	private double meridiano;
+	private double paralelo;
+	private double distancia_tierra;
+	@Test
+	public void testConstructor1() {
+		meridiano = 107;
+		paralelo = 200;
+		distancia_tierra = 500;
+		Satelite sateliteTest1 = new Satelite(meridiano,paralelo,distancia_tierra);
+		
+		assertEquals(sateliteTest1.meridiano,107,0);
+		assertEquals(sateliteTest1.paralelo,200,0);
+		assertEquals(sateliteTest1.distancia_tierra,500,0);
+	}
+	
+	@Test
+	public void testConstructor2() {
+		Satelite sateliteTest2 = new Satelite();
+		
+		assertEquals(sateliteTest2.distancia_tierra,0,0);
+		assertEquals(sateliteTest2.paralelo,0,0);
+		assertEquals(sateliteTest2.meridiano,0,0);
+	
+	}
+	@Test
+	public void setPosicionTest()
+	{
+		double meridianoTest = 345;
+		double paraleloTest = 123;
+		double distancia_tierraTest = 567;
+		
+		meridiano = 11;
+		paralelo = 22 ;
+		distancia_tierra = 44;
+		Satelite sateliteTest3 = new Satelite(meridiano,paralelo,distancia_tierra);
+		
+		sateliteTest3.setPosicion(meridianoTest,paraleloTest,distancia_tierraTest);
+		assertEquals(sateliteTest3.meridiano,345,0);
+		assertEquals(sateliteTest3.paralelo,123,0);
+		assertEquals(sateliteTest3.distancia_tierra,567,0);
+	}
+}
